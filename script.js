ab631432deb428d7f56aeff3352afe6996234f09
tkinthebay <61370275+tkinthebay@users.noreply.github.com>
2024-02-05 16:19:41 -0600

    Added basic calculator functions
    Addition, subtraction, multiplication, and division

---
+++ script.js
@@ -0,0 +1,16 @@
+// basic calculator functions
+function add (x,y) {
+    return x+y;
+}
+
+function subtract (x,y) {
+    return x-y;
+}
+
+function multiply (x,y) {
+    return x*y;
+}
+
+function divide (x,y) {
+    return x/y;
+}
