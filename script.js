93464dd4e39a2c57e45bb68b0f46e87014548f4a
tkinthebay <61370275+tkinthebay@users.noreply.github.com>
2024-02-05 18:18:02 -0600

    Set variables that point to elements in HTML

--- script.js
+++ script.js
@@ -2,6 +2,26 @@
 // let firstNum = ;
 // let secondNum = ;
 // let operator = ;
+const zero = document.getElementById("zero");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const clear = document.getElementById("clear");
+const decimal = document.getElementById("decimal");
+const addition = document.getElementById("addition");
+const subtraction = document.getElementById("subtraction");
+const multiplication = document.getElementById("multiplication");
+const division = document.getElementById("division");
+const equals = document.getElementById("equals");
+const result = document.getElementById("result");
+
+
 
 // create calculator functions
 function add(x, y) {
